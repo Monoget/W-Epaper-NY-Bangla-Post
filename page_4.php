e8c7336be503f317f06544663762ab3ef9a6c5fd
Monoget <monoget2@gmail.com>
2022-01-14 02:52:09 +0600

    update newspaper

--- page_4.php
+++ page_4.php
@@ -18,44 +18,25 @@
         </div>
         <div class="seperator_15"></div>
         <a class="thumbnail imagethumb" target="_blank"
-           href="paper_image/page_image/CTB-03-04%20(11-04-2022_1_page.jpg"><img
-                    src="paper_image/page_image/CTB-03-04%20(11-04-2022_1_page.jpg"
+           href="paper_image/page_image/4.png"><img
+                    src="paper_image/page_image/4.png"
                     alt=""/></a>
-        <style media="print">
-            body * {
-                display: none;
-            }
-
-            body #print_img {
-                display: block !important;
-            }
-        </style>
-        <img class="visible-print" id="print_img"
-             src="paper_image/page_image/imageprocessor8623.gif"
-             alt=""/>
-        <script>
-            if ($(window).width() > 768) {
-                $('.imagethumb').colorbox();
-            }
-            $("body > #print_img").remove();
-            $("body").prepend($("#print_img"));
-        </script>
     </div>
     <div class="col-xs-3">
         <div class="seperator_15"></div>
 
         <section class='widget'>
             <header class='widget_title'><h1>Advertise here</h1></header>
-            <div class='widget_body'><a href="#"><!--<img style="width: 100%; height: auto;"
-                                                                      src="img/pnp_ads_300x600.jpg"
-                                                                      alt="Side Ads"/>--></a>
+            <div class='widget_body'><a href="#"><img style="width: 100%; height: auto;"
+                                                      src="img/pnp_ads_300x600.png"
+                                                      alt="Side Ads"/></a>
                 <br><br></div>
         </section>
 
         <section class='widget'>
-            <div class='widget_body'><a href="index.html" target="blank"><!--<img
-                                    style="width: 100%; height: 100%;" src="img/pnp-epaper-logo.jpg"
-                                    alt="epaper"/>--></a>
+            <div class='widget_body'><a href="index.html" target="blank"><img
+                            style="width: 100%; height: 100%;" src="img/Driving_School.png"
+                            alt="epaper"/></a>
 
                 <br><br></div>
         </section>
@@ -64,15 +45,13 @@
             <header class='widget_title'><h1>Subscribe Here</h1></header>
             <div class='widget_body'>
                 <form style="border:1px solid #ccc;padding:3px;text-align:center;"
-                      action="https://feedburner.google.com/fb/a/mailverify" method="post"
-                      target="popupwindow"
-                      onsubmit="window.open('https://feedburner.google.com/fb/a/mailverify?uri=purbanchal/rss', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true">
+                      action="" method="post">
                     <p>Enter your email address:</p>
                     <p><input type="text" style="width:140px" name="email"/></p><input type="hidden"
                                                                                        value="purbanchal/rss"
                                                                                        name="uri"/><input
                             type="hidden" name="loc" value="en_US"/><input type="submit" value="Subscribe"/>
-                    <p>Delivered by <a href="https://feedburner.google.com/" target="_blank">FeedBurner</a>
+                    <p>Delivered by NY Bangla Post
                     </p></form>
             </div>
         </section>
@@ -83,5 +62,3 @@
 <?php require_once 'includes/footer.php'; ?>
 </body>
 </html>
-
-
